c40d0b7fa14477961859ca18a58309bd585b5928
Michael <x30827pos@gmail.com>
2024-10-13 06:21:53 +0300

    Update README.md and exploration_nb.ipynb

--- src/model.py
+++ src/model.py
@@ -1,3 +1,5 @@
+from typing import List, Optional, Tuple
+
 import torch
 from super_gradients.training import Trainer, models
 from super_gradients.training.losses import PPYoloELoss
@@ -5,18 +7,77 @@ from super_gradients.training.metrics import DetectionMetrics_050
 from super_gradients.training.models.detection_models.pp_yolo_e import (
     PPYoloEPostPredictionCallback,
 )
+from torch import nn
+from torch.utils.data import DataLoader
 
 
-def load_model(num_classes=2, pretrained=True):
-    if pretrained:
-        model = models.get("yolo_nas_l", pretrained_weights="coco")
-    else:
+def load_model(
+    num_classes: int = 2,
+    pretrained: bool = True,
+    input_size: Tuple[int, int] = (640, 640),
+    path: Optional[str] = None,
+) -> nn.Module:
+    """
+    Загрузка модели YOLO NAS с возможностью загрузки предобученных весов или сохраненной модели.
+
+    :param num_classes: Количество классов.
+    :param pretrained: Использовать предобученные веса.
+    :param input_size: Размер входного изображения.
+    :param path: Путь к файлу с сохраненной моделью (если есть).
+    :return: Модель YOLO NAS.
+    """
+    if path:
+        # Если указан путь к файлу, загружаем сохраненную модель
         model = models.get("yolo_nas_l", num_classes=num_classes)
-    model.prep_model_for_conversion(input_size=(640, 640))
+        model.load_state_dict(torch.load(path))
+        model.eval()
+    else:
+        # Иначе загружаем новую модель с предобученными весами или без них
+        if pretrained:
+            model = models.get("yolo_nas_l", pretrained_weights="coco")
+        else:
+            model = models.get("yolo_nas_l", num_classes=num_classes)
+        model.prep_model_for_conversion(input_size=input_size)
+
     return model
 
 
-def train_model(train_loader, val_loader, num_classes=2, epochs=100, lr=0.001):
+def train_model(
+    train_loader: DataLoader,
+    val_loader: DataLoader,
+    num_classes: int = 2,
+    epochs: int = 100,
+    lr: float = 0.001,
+    experiment_name: str = "aircraft_detection",
+    ckpt_root_dir: str = "models",
+    score_threshold: float = 0.1,
+    nms_threshold: float = 0.6,
+    nms_top_k: int = 1000,
+    max_predictions: int = 300,
+    lr_mode: str = "StepLRScheduler",
+    lr_updates: List[int] = [70, 90],
+    lr_decay_factor: float = 0.1,
+) -> nn.Module:
+    """
+    Обучение модели YOLO NAS.
+
+    :param train_loader: DataLoader для обучающего набора данных.
+    :param val_loader: DataLoader для валидационного набора данных.
+    :param num_classes: Количество классов.
+    :param epochs: Количество эпох обучения.
+    :param lr: Начальная скорость обучения.
+    :param experiment_name: Имя эксперимента.
+    :param ckpt_root_dir: Директория для сохранения контрольных точек.
+    :param score_threshold: Порог уверенности для постобработки.
+    :param nms_threshold: Порог NMS (Non-Maximum Suppression).
+    :param nms_top_k: Максимальное количество предсказаний после NMS.
+    :param max_predictions: Максимальное количество предсказаний.
+    :param lr_mode: Режим изменения скорости обучения.
+    :param lr_updates: Эпохи изменения скорости обучения.
+    :param lr_decay_factor: Коэффициент уменьшения скорости обучения.
+    :return: Обученная модель YOLO NAS.
+    """
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = models.get(
@@ -24,23 +85,23 @@ def train_model(train_loader, val_loader, num_classes=2, epochs=100, lr=0.001):
     ).to(device)
 
     trainer = Trainer(
-        experiment_name="aircraft_detection", ckpt_root_dir="models"
+        experiment_name=experiment_name, ckpt_root_dir=ckpt_root_dir
     )
 
     post_prediction_callback = PPYoloEPostPredictionCallback(
-        score_threshold=0.1,
-        nms_threshold=0.6,
-        nms_top_k=1000,
-        max_predictions=300,
+        score_threshold=score_threshold,
+        nms_threshold=nms_threshold,
+        nms_top_k=nms_top_k,
+        max_predictions=max_predictions,
     )
 
     trainer.train(
         model=model,
         training_params={
             "max_epochs": epochs,
-            "lr_mode": "StepLRScheduler",
-            "lr_updates": [70, 90],
-            "lr_decay_factor": 0.1,
+            "lr_mode": lr_mode,
+            "lr_updates": lr_updates,
+            "lr_decay_factor": lr_decay_factor,
             "initial_lr": lr,
             "loss": PPYoloELoss(num_classes=num_classes),
             "optimizer": "Adam",
@@ -67,18 +128,19 @@ def train_model(train_loader, val_loader, num_classes=2, epochs=100, lr=0.001):
     return model
 
 
-def save_model(model, path):
+def save_model(model: nn.Module, path: str) -> None:
+    """
+    Сохранение модели в указанный путь.
+
+    :param model: Модель для сохранения.
+    :param path: Путь для сохранения модели.
+    """
     model.save(path)
 
 
-def load_trained_model(path, num_classes=2):
-    model = models.get("yolo_nas_l", num_classes=num_classes)
-    model.load_state_dict(torch.load(path))
-    model.eval()
-    return model
-
-
-def evaluate_model(model, test_loader, num_classes=2):
+def evaluate_model(
+    model: nn.Module, test_loader: DataLoader, num_classes: int = 2
+) -> float:
     """
     Оценка модели на тестовом наборе данных с использованием метрики mAP@0.50.
 
